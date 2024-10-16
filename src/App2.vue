98b36f554ca0f54e691175a5691f4dd885d5150d
Muhammad Haram <haramh643@gmail.com>
2024-10-17 01:34:13 +0500

    commit

---
+++ src/App2.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    data() {
+        return {
+            name: "Muhammad Haram",
+            status: "active",
+            tasks: ['1', '2', '3', '4', '5'],
+            link: "https://github.com/"
+        };
+    },
+    methods: {
+        toggleStatus() {
+            if (this.status === "active") {
+                this.status = "pending";
+            } else if (this.status === "pending") {
+                this.status = "inactive";
+            } else {
+                this.status = "active";
+            }
+        },
+    }
+}
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+    <p v-if="status === 'active'">User is Active</p>
+    <p v-else-if="status === 'pending'">User is pending</p>
+    <p v-else>User is not active</p>
+    <h1>tasks</h1>
+    <ul>
+        <li v-for="task in tasks" :key='task'>{{ task }}</li>
+    </ul>
+
+    <a v-bind:href="link">github</a>
+    <button @click="toggleStatus">click</button>
+
+</template>
