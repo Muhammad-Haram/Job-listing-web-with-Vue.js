43a1597c5f0647e7b77fd151f868dfc8b41e06b0
Muhammad Haram <haramh643@gmail.com>
2024-10-17 02:28:57 +0500

    todo list demo

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import { ref } from 'vue'
 const name = ref("Muhammmad Haram");
 const status = ref("active");
 const tasks = ref(["task1", "task2", "task3"]);
+const newTask = ref("")
 
 const toggleStatus = () => {
     if (status.value === "active") {
@@ -15,6 +16,18 @@ const toggleStatus = () => {
         status.value = "active";
     }
 }
+
+const addTask = () => {
+    if (newTask.value.trim() !== "") {
+        tasks.value.push(newTask.value);
+        newTask.value = ""
+    }
+}
+
+const deleteTask = (index) => {
+    tasks.value.splice(index, 1);
+}
+
 </script>
 
 <template>
@@ -24,9 +37,18 @@ const toggleStatus = () => {
     <p v-else>User is not active</p>
     <h1>tasks</h1>
     <ul>
-        <li v-for="task in tasks" :key='task'>{{ task }}</li>
+        <li v-for="(task, index) in tasks" :key='task'>
+            <span>{{ task }}</span>
+            <button @click="deleteTask(index)">x</button>
+        </li>
     </ul>
 
     <button @click="toggleStatus">click</button>
+
+    <form @submit.prevent="addTask">
+        <label for="newTask"></label>
+        <input type="text" id="newTask" name="newTask" v-model="newTask" />
+        <button type="submit">Submit</button>
+    </form>
 
 </template>
