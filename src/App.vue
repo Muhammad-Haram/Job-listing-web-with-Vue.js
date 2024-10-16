7623398de2366b361f2d73a62dbc327d28b87bb4
Muhammad Haram <haramh643@gmail.com>
2024-10-17 00:33:51 +0500

    learn

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            name: "Muhammad Haram",
+            status: "active",
+            tasks: ['1', '2', '3', '4', '5'],
+            link : "https://github.com/"
+        }
+    },
+}
+</script>
+
 <template>
-<h1>Hello Vue</h1>
+    <h1>{{ name }}</h1>
+    <p v-if="status === 'active'">User is Active</p>
+    <p v-else-if="status === 'pending'">User is pending</p>
+    <p v-else>User is not active</p>
+    <h1>tasks</h1>
+    <ul>
+        <li v-for="task in tasks" :key = 'task'>{{ task }}</li>
+    </ul>
+
+    <a v-bind:href="link">github</a>
+
 </template>
