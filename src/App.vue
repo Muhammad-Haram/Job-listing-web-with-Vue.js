466075c36e8a9bb8b1cc10c43bcc6f64d9339afc
Muhammad Haram <haramh643@gmail.com>
2024-10-18 01:55:59 +0500

    hero card sec

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
+import HeroCards from './components/HeroCards.vue';
 </script>
 
 <template>
     <Navbar />
     <Hero title="Job Listing Website" />
+    <HeroCards/>
 </template>
